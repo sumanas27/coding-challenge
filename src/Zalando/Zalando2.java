674e3cad4a663a4d7f94df1ab19b72c949950943
@sumanas27 <sumanas27@gmail.com>
2021-04-29 15:39:00 +0200

    removed the commented lines

--- src/Zalando/Zalando2.java
+++ src/Zalando/Zalando2.java
@@ -10,13 +10,6 @@ import java.util.stream.Collectors;
 public class Zalando2 {
     public int solution(int[] A) {
         // write your code in Java SE 8
-//        int totalNoPairs = 0;
-//
-//        Map<Integer,Integer> arrayIndexValueMap = new HashMap<>();
-//
-//        for( int i = 0; i < A.length; i++ ){
-//            arrayIndexValueMap.put( i, A[i] );
-//        }
 
         Map<Integer, Long> arrayIndexValueMap = Arrays.stream(A)
                 .boxed()
@@ -26,8 +19,6 @@ public class Zalando2 {
                 .stream()
                 .mapToInt( Zalando2::countPair ).sum();
 
-
-        //return totalNoPairs;
     }
     public static int countPair(Map.Entry<Integer,Long> entry){
         return ((entry.getValue().intValue() - 1)) * entry.getValue().intValue() / 2;
